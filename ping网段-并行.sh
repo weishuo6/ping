d0fbf2e008bd1e7fd3bad6dc4ad54d910515e705
weishuo6 <35896106+weishuo6@users.noreply.github.com>
2019-01-08 15:26:19 +0800

    并行ping速度更快

--- ping网段-并行.sh
+++ ping网段-并行.sh
@@ -1,11 +1,15 @@
-for ip in 192.168.0.{1..255} ;
-do
-(
-ping $ip -c2 &> /dev/null ;
-if [ $? -eq 0 ];
-then
-echo $ip is alive
-fi
-)&
-done
+#!/bin/bash
+for ip in 192.168.222.{1..10};
+do 
+  ( 
+     ping $ip -c 2 &> /dev/null;
+     if [ $? -eq 0 ];
+     then
+       echo $ip is alive  >> alive.txt
+     else
+       echo $ip is down   >> down.txt
+     fi
+   )&
+   done
 wait
+#要使ping命令可以并行执行，可将循环体放入()&。()中的命令会在子shell中运行，而&会将其置入后台。
